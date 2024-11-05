9969eb0ccf351e03d032abba39ec63bb2d6bb4be
melinaSQ <melinasalazarq@gmail.com>
2024-11-05 12:55:06 -0400

    configuraacion de socket io sin secretos

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,16 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { RolesModule } from './roles/roles.module';
-/*
 import { SocketModule } from './socket/socket.module';
-import { DriversPositionModule } from './drivers_position/drivers_position.module';
-import { ClientRequestsModule } from './client_requests/client_requests.module';
-import { TimeAndDistanceValuesModule } from './time_and_distance_values/time_and_distance_values.module';
-import { DriverTripOffersModule } from './driver_trip_offers/driver_trip_offers.module';
-import { DriverCarInfoModule } from './driver_car_info/driver_car_info.module';
-import { ConfigModule } from '@nestjs/config';
-import { FirebaseModule } from './firebase/firebase.module';
-import { AuthModule } from './auth/auth.module';
-*/
+// import { DriversPositionModule } from './drivers_position/drivers_position.module';
+// import { ClientRequestsModule } from './client_requests/client_requests.module';
+// import { TimeAndDistanceValuesModule } from './time_and_distance_values/time_and_distance_values.module';
+// import { DriverTripOffersModule } from './driver_trip_offers/driver_trip_offers.module';
+// import { DriverCarInfoModule } from './driver_car_info/driver_car_info.module';
+// import { ConfigModule } from '@nestjs/config';
+// import { FirebaseModule } from './firebase/firebase.module';
+// import { AuthModule } from './auth/auth.module';
+
 
 
 @Module({
@@ -33,6 +32,7 @@ import { AuthModule } from './auth/auth.module';
     UsersModule,
     AuthModule,
     RolesModule,
+    SocketModule,
     /*
     ConfigModule.forRoot({ cache: true }),
     SocketModule,
